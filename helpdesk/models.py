0f679c399778d9b1b1a0323a72dda6ea6999b34e
Raihan Tusher <raihan.tusher@yahoo.com>
2023-02-17 17:10:32 +0600

    Tickets and Comments model

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    STATUS = (
+        ('Unassigned', 'Unasigned'),
+        ('Assigned', 'Assigned'),
+        ('Completed', 'Completed'),
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS, default="Unassigned")
+    datetime = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    ticket  = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    #by = models
