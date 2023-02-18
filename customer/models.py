e3cb80c7ca1cb7ad66070d1727e250be75f56988
Raihan Tusher <raihan.tusher@yahoo.com>
2023-02-18 19:27:07 +0600

    customer package

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+class Customer(models):
+    full_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=14)
+    pppoe_username = models.CharField(max_length=25)
+    pppoe_password = models.CharField(max_length=25)
+class Area(models):
+    area_name = models.CharField(max_length=25)
