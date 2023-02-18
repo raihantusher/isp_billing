a47841e2b30c7b34a6feee9707ac361d526832a3
Raihan Tusher <raihan.tusher@yahoo.com>
2023-02-18 19:27:13 +0600

    customer package

--- package/models.py
+++ package/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Package(models.Model):
+    package_name = models.CharField(max_length=200)
+    price = models.DecimalField(default=0)
