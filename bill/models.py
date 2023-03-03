4a6c517dae8cb20e5e60ca3232679a83b56d6925
Raihan Tusher <raihan.tusher@yahoo.com>
2023-03-03 08:47:11 +0600

    bill models created

--- bill/models.py
+++ bill/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from package.models import Package
+
 
 # Create your models here.
+
+class Bill(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    due = models.IntegerField(default=0)
+    is_paid = models.BooleanField(default=False)
+    due_date = models.DateTimeField(auto_now_add=True)
+    paid_date = models.DateTimeField(auto_now=False)
